4e4260d8a1538b6f0f3a9454a35d52fb5be53927
Ixtli <cg@325i.org>
2011-02-26 20:30:19 -0500

    Added interface.js

---
+++ interface.js
@@ -0,0 +1,56 @@
+function Interface()
+{
+    // Array of interface objects
+    this.iobjs = [];
+}
+
+Interface.prototype = {
+    addObject: function(object)
+    {
+        
+    },
+    
+    removeObject: function(object)
+    {
+        
+    },
+    
+    moveObjectCloser: function(object)
+    {
+        
+    },
+    
+    moveObjectFurther: function(object)
+    {
+        
+    },
+    
+    moveObjectToFront: function(object)
+    {
+        
+    },
+    
+    moveObjectToBack: function(object)
+    {
+        
+    },
+    
+};
+
+function InterfaceObject(name, px, py, width, height)
+{
+    this.name = name;
+    
+    // Geometry
+    this.px = px;
+    this.py = py;
+    this.width = width;
+    this.height = height;
+    
+    
+}
+
+InterfaceObject.prototype = {
+    
+};
+
